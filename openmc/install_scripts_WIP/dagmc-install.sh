8a8c8003ee3006e068901e70d13a006616cbf7eb
luke.labrie <lplcleary@gmail.com>
2021-11-21 15:53:56 +0000

    main:main

--- openmc/install_scripts_WIP/dagmc-install.sh
+++ openmc/install_scripts_WIP/dagmc-install.sh
@@ -9,17 +9,17 @@ set -ex
 echo "Compiled & installed double-down, proceeding..."
 
 cd $HOME
-mkdir DAGMC
+#mkdir DAGMC
 cd DAGMC
-git clone --single-branch --branch develop --depth 1 https://github.com/svalinn/DAGMC.git
-mkdir build
+#git clone --single-branch --branch develop --depth 1 https://github.com/svalinn/DAGMC.git
+#mkdir build
 cd build
 cmake ../DAGMC -DBUILD_TALLY=ON \
-               -DMOAB_DIR=/MOAB \
+               -DMOAB_DIR=$HOME/MOAB \
                -DDOUBLE_DOWN="$include_double_down" \
                -DBUILD_STATIC_EXE=OFF \
                -DBUILD_STATIC_LIBS=OFF \
-               -DCMAKE_INSTALL_PREFIX=/DAGMC/ \
-               -DDOUBLE_DOWN_DIR=/double-down
+               -DCMAKE_INSTALL_PREFIX=$HOME/DAGMC/ \
+               -DDOUBLE_DOWN_DIR=$HOME/double-down
 make install
 rm -rf /DAGMC/DAGMC /DAGMC/build
--- openmc/install_scripts_WIP/double_down-install.sh
+++ openmc/install_scripts_WIP/double_down-install.sh
@@ -4,6 +4,8 @@
 #!/bin/bash
 set -ex
 
+sudo pacman -Syu --noconfirm doxygen
+
 #embree compile & install
 ./embree-install.sh
 echo "Compiled & installed embree, proceeding..."
@@ -13,14 +15,14 @@ echo "Compiled & installed embree, proceeding..."
 echo "Compiled & installed moab, proceeding..."
 
 cd $HOME
-mkdir double-down
+#mkdir double-down
 cd double-down
-git clone --single-branch --branch main --depth 1 https://github.com/pshriwise/double-down.git
-mkdir build
+#git clone --single-branch --branch main --depth 1 https://github.com/pshriwise/double-down.git
+#mkdir build
 cd build
-cmake ../double-down -DMOAB_DIR=/MOAB \
-                     -DCMAKE_INSTALL_PREFIX=/double-down \
-                     -DEMBREE_DIR=/embree
+cmake ../double-down -DMOAB_DIR=$HOME/MOAB \
+                     -DCMAKE_INSTALL_PREFIX=$HOME/double-down \
+                     -DEMBREE_DIR=$HOME/embree
 make
 make install
 rm -rf /double-down/build /double-down/double-down
