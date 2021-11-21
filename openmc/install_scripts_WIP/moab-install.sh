9c3561d562ea3bd79ad7ee2c12233007b8ebc6ef
luke.labrie <lplcleary@gmail.com>
2021-11-21 15:38:04 +0000

    main:main

--- openmc/install_scripts_WIP/moab-install.sh
+++ openmc/install_scripts_WIP/moab-install.sh
@@ -7,19 +7,19 @@ set -ex
 sudo pacman -Syu --noconfirm eigen \
 		   netcdf \
 		   hdf5 \
-		   python-cython
+		   cython
 
 cd $HOME
-mkdir MOAB
+#mkdir MOAB
 cd MOAB
-git clone  --single-branch --branch 5.3.0 --depth 1 https://bitbucket.org/fathomteam/moab.git
-mkdir build
+#git clone  --single-branch --branch 5.3.0 --depth 1 https://bitbucket.org/fathomteam/moab.git
+#mkdir build
 cd build
 cmake ../moab -DENABLE_HDF5=ON \
               -DENABLE_NETCDF=ON \
               -DENABLE_FORTRAN=OFF \
               -DENABLE_BLASLAPACK=OFF \
-              -DBUILD_SHARED_LIBS=OFF \
+              -DBUILD_SHARED_LIBS=ON \
               -DCMAKE_INSTALL_PREFIX=$HOME/MOAB
 make
 make install 
@@ -32,4 +32,4 @@ cmake ../moab -DENABLE_HDF5=ON \
 make install
 cd pymoab
 bash install.sh
-python setup.py install
+sudo python setup.py install
