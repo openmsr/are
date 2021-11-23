66d2fb5281852452857ad0c903f7882bbd6af657
Luke Labrie-Cleary <89009042+LukeLabrie@users.noreply.github.com>
2021-11-23 16:05:10 +0000

    Update install-all.sh

--- openmc/install_scripts_WIP/install-all.sh
+++ openmc/install_scripts_WIP/install-all.sh
@@ -5,3 +5,6 @@ set -ex
 #openmc-install.sh will call install scripts of its dependencies & nuclear data
 ./openmc-install.sh
 echo "Compiled & installed openmc, done."
+
+echo "Running test script..."
+python test_openmc.py
