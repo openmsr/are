ce6d04d518f48669b73a92593cfb6e4a360b842a
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-02-04 09:15:27 +0100

    no need to sudo this it's in the home directory anyway

--- openmc/install_scripts/nuclear_data-install.sh
+++ openmc/install_scripts/nuclear_data-install.sh
@@ -4,7 +4,7 @@
 #!/bin/bash
 set -ex
 
-sudo echo "export OPENMC_CROSS_SECTIONS=$HOME/openmc/nuclear_data/endfb71_hdf5/cross_sections.xml" >> $HOME/.bashrc
+echo "export OPENMC_CROSS_SECTIONS=$HOME/openmc/nuclear_data/endfb71_hdf5/cross_sections.xml" >> $HOME/.bashrc
 source $HOME/.bashrc
 
 #defaul libraries
@@ -13,7 +13,7 @@ endfb_VIII="https://anl.box.com/shared/static/uhbxlrx7hvxqw27psymfbhi7bx7s6u6a.x
 jeff="https://anl.box.com/shared/static/4jwkvrr9pxlruuihcrgti75zde6g7bum.xz"
 
 cd $HOME/openmc
-sudo mkdir nuclear_data
+mkdir -p nuclear_data
 cd nuclear_data
 
 #see other default options above
