3b98874387ce9966111001a41fd14dc6aca8709f
LukeLabrie <lplcleary@gmail.com>
2022-07-13 20:02:30 +0200

    updated syntax for cad_to_openmc

--- step_to_h5m/step_to_h5m.py
+++ step_to_h5m/step_to_h5m.py
@@ -4,23 +4,17 @@
 import numpy as np
 import os
 from sys import path
-
+path_to_assembly = os.getcwd() + '/../CAD_to_OpenMC/'
+sys.path.append(path_to_assembly)
+import CAD_to_OpenMC.assembly as ab
 ###############################################################################
-#inputs
 
-# get assembly module
-path_to_assembly = os.getcwd() + '/../step_to_h5m/src/h5massembly'
-path.append(path_to_assembly)
-import assembly as ab
-
+# inputs
+step_filepath = "./step_files/ARE.step"
 h5m_out_filepath = os.getcwd() + '/h5m_files/ARE.h5m'
 
 a=ab.Assembly()
-a.stp_files=[os.getcwd()+"/step_files/ARE_actual.step"]
-a.verbose=2
+a.verbose=10
+a.stp_files=[step_filepath]
 a.import_stp_files()
-a.export_brep('ARE.brep')
-a.brep_to_h5m(brep_filename='ARE.brep',
-              h5m_filename=h5m_out_filepath,
-              backend='stl',
-              threads=6)
+a.solids_to_h5m(backend='stl',h5m_filename=h5m_out_filepath,threads=6)
