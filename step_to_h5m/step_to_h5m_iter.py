d18cd8e7e5648c2ce32f20c768d441aaf5c6e13b
LukeLabrie <lplcleary@gmail.com>
2022-07-19 16:59:07 +0200

    parametric mesh increase

--- step_to_h5m/step_to_h5m_iter.py
+++ step_to_h5m/step_to_h5m_iter.py
@@ -11,23 +11,32 @@ import CAD_to_OpenMC.assembly as ab
 ###############################################################################
 
 # inputs
-step_filepath = "./step_files/test_mesh.step"
-h5m_out_filepath = os.getcwd() + '/h5m_files/test_mesh_3_50_0.001.h5m'
+step_filepath = "./step_files/ARE_2.step"
 
+iters = 2000
+i = 0
 # mesher config
 ab.mesher_config['min_mesh_size'] = 0.001
 ab.mesher_config['max_mesh_size'] = 100
 ab.mesher_config['curve_samples'] = 50
-ab.mesher_config['threads'] = 3
-ab.mesher_config['mesh_algorithm'] = 6
-#ab.mesher_config['vetoed'] = [474,1157,1243,1341,1537]
+ab.mesher_config['threads'] = 55
+ab.mesher_config['mesh_algorithm'] = 2
+ab.mesher_config['vetoed'] = [1157,1159,1243,1341,1537]
+while i<iters:
+    h5m_out_filepath = os.getcwd() + '/h5m_files/ARE_try_' + str(i) + '.h5m'
+    # output
+    try:
+        a=ab.Assembly()
+        a.verbose=10
+        a.stp_files=[step_filepath]
+        a.import_stp_files()
+        a.solids_to_h5m(backend='gmsh',h5m_filename=h5m_out_filepath)
+    except:
+        ab.mesher_config['min_mesh_size'] += 0.001
+    i += 1
+
+
+
 #ab.mesher_config['angular_tolerance'] = 0.09
 
 #ab.mesher_config['tolerance'] = 0.001
-
-# output
-a=ab.Assembly()
-a.verbose=10
-a.stp_files=[step_filepath]
-a.import_stp_files()
-a.solids_to_h5m(backend='gmsh',h5m_filename=h5m_out_filepath)
