6189fb3014e979e364e846b4e2d72fee951fe3e2
LukeLabrie <lplcleary@gmail.com>
2022-07-20 15:33:50 +0200

    fixed outlet

--- step_to_h5m/step_to_h5m.py
+++ step_to_h5m/step_to_h5m.py
@@ -11,15 +11,15 @@ import CAD_to_OpenMC.assembly as ab
 ###############################################################################
 
 # inputs
-step_filepath = "./step_files/test_mesh.step"
-h5m_out_filepath = os.getcwd() + '/h5m_files/test_mesh_3_50_0.001.h5m'
+step_filepath = "./step_files/ARE_3.step"
+h5m_out_filepath = os.getcwd() + '/h5m_files/ARE.h5m'
 
 # mesher config
-ab.mesher_config['min_mesh_size'] = 0.001
-ab.mesher_config['max_mesh_size'] = 100
-ab.mesher_config['curve_samples'] = 50
-ab.mesher_config['threads'] = 3
-ab.mesher_config['mesh_algorithm'] = 6
+ab.mesher_config['min_mesh_size'] = 0.49
+ab.mesher_config['max_mesh_size'] = 1000
+ab.mesher_config['curve_samples'] = 100
+ab.mesher_config['threads'] = 6
+ab.mesher_config['mesh_algorithm'] = 2
 #ab.mesher_config['vetoed'] = [474,1157,1243,1341,1537]
 #ab.mesher_config['angular_tolerance'] = 0.09
 
