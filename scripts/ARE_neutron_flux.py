dce9776498548e925ce2fe32390600747c218ab4
Luke Labrie-Cleary <89009042+LukeLabrie@users.noreply.github.com>
2022-07-28 09:52:44 +0200

    Update ARE_neutron_flux.py

--- scripts/ARE_neutron_flux.py
+++ scripts/ARE_neutron_flux.py
@@ -8,34 +8,29 @@ from materials import *
 
 #Geometry
 h5m_filepath = 'h5m_files/ARE.h5m'
-
-#materials
-mats = openmc.Materials([salt,BeO,inconel,insulation,coolant,helium,stainless,boron,blanket,shield])
-mats.export_to_xml()
-
-settings = openmc.Settings()
-settings.temperature = {'method':'interpolation'}
-settings.batches = 100
-settings.inactive = 10
-settings.particles = 5000
-settings.max_lost_particles = 10000
-source_area = openmc.stats.Box([-200., -200., -200.],[ 200.,  200.,  200.],only_fissionable = True)
-settings.source = openmc.Source(space=source_area)
-settings.export_to_xml()
-
-#geometry
 graveyard=openmc.Sphere(r=10000,boundary_type='vacuum')
-
 cad_univ = openmc.DAGMCUniverse(filename=h5m_filepath,auto_geom_ids=True,universe_id=996 )
-
 cad_cell = openmc.Cell(cell_id=997 , region= -graveyard, fill= cad_univ)
-
 root = openmc.Universe(universe_id=998)
 root.add_cells([cad_cell])
 geometry = openmc.Geometry(root)
 geometry.export_to_xml()
 
+#materials
+mats = openmc.Materials([salt,BeO,inconel,insulation,coolant,helium,stainless,boron,blanket,shield])
+mats.export_to_xml()
 
+#settings
+settings = openmc.Settings()
+settings.temperature = {'method':'interpolation'}
+settings.batches = 100
+settings.inactive = 10
+settings.particles = 5000
+source_area = openmc.stats.Box([-200., -200., -200.],[ 200.,  200.,  200.],only_fissionable = True)
+settings.source = openmc.Source(space=source_area)
+settings.export_to_xml()
+
+#tallies
 tallies = openmc.Tallies()
 
 mesh = openmc.RegularMesh()
