e2b66555cb8c2278cb7747d9449297603a7de630
LukeLabrie <lplcleary@gmail.com>
2022-09-13 16:37:03 +0200

    further edits

--- scripts/step_to_h5m.py
+++ scripts/step_to_h5m.py
@@ -10,13 +10,13 @@ import CAD_to_OpenMC.assembly as ab
 
 # inputs
 step_filepath = "./step_files/are_no_control_rods.step"
-h5m_out_filepath = os.getcwd() + '/h5m_files/ARE_mesh_refine.h5m'
+h5m_out_filepath = os.getcwd() + '/h5m_files/ARE_open_no_heal.h5m'
 
 # mesher config
-ab.mesher_config['refine'] = True
+#ab.mesher_config['refine'] = True
 #ab.mesher_config['min_mesh_size'] = 0.001
 #ab.mesher_config['mesh_algorithm'] = 2
-#ab.mesher_config['curve_samples'] = 100
+ab.mesher_config['curve_samples'] = 50
 #ab.mesher_config['threads'] = 6
 #ab.mesher_config['max_mesh_size'] = 1000
 #ab.mesher_config['vetoed'] = [474,1157,1243,1341,1537]
