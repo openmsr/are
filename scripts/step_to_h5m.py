cb6dfcf6c6c34229fabc0771b9f47035c86aee47
lukelabrie <lplcleary@gmail.com>
2022-12-20 17:08:58 +0100

    minor updates

--- scripts/step_to_h5m.py
+++ scripts/step_to_h5m.py
@@ -9,16 +9,16 @@ import CAD_to_OpenMC.assembly as ab
 ###############################################################################
 
 # inputs
-step_filepath = "./step_files/are_no_control_rods.step"
-h5m_out_filepath = os.getcwd() + '/h5m_files/ARE_mmg_refine.h5m'
+step_filepath = "./step_files/are_rods_35.step"
+h5m_out_filepath = os.getcwd() + '/h5m_files/ARE_no_heal.h5m'
 
 # mesher config
-ab.mesher_config['refine'] = True
-ab.mesher_config['threads'] = 7
-ab.mesher_config['angular_tolerance'] = 0.1
-#ab.mesher_config['curve_samples'] = 40
+#ab.mesher_config['refine'] = True
+ab.mesher_config['threads'] = 55
+#ab.mesher_config['angular_tolerance'] = 0.1
+ab.mesher_config['curve_samples'] = 30
 #ab.mesher_config['min_mesh_size'] = 0.001
-#ab.mesher_config['mesh_algorithm'] = 2
+ab.mesher_config['mesh_algorithm'] = 2
 #ab.mesher_config['max_mesh_size'] = 1000
 #ab.mesher_config['vetoed'] = [474,1157,1243,1341,1537]
 #ab.mesher_config['tolerance'] = 0.001
@@ -27,4 +27,4 @@ ab.mesher_config['angular_tolerance'] = 0.1
 a=ab.Assembly()
 a.stp_files=[step_filepath]
 a.import_stp_files()
-a.solids_to_h5m(backend='stl',h5m_filename=h5m_out_filepath)
+a.solids_to_h5m(backend='gmsh',h5m_filename=h5m_out_filepath,heal=False)
