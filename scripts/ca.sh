3ca60862845453c11caabfbfbd4acafd8c966137
lukelabrie <lplcleary@gmail.com>
2023-03-13 15:52:20 +0100

    writeup updates

--- scripts/ca.sh
+++ scripts/ca.sh
@@ -6,10 +6,10 @@ set -ex
 rm -f tallies.xml
 
 if test -f "./h5m_files/ARE_rods_35.h5m"; then
-  python ./scripts/ARE_criticality_approach.py
+  python ./scripts/ARE_criticality_approach_2.py
   python ./scripts/ARE_criticality_approach_post.py
 else
   python ./scripts/step_to_h5m_cubit.py ./step_files/ARE_rods_35.step
-  python ./scripts/ARE_criticality_approach.py
+  python ./scripts/ARE_criticality_approach_2.py
   python ./scripts/ARE_criticality_approach_post.py
 fi
