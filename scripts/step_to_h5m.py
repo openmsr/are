6e4d447fd88b0b5aea7c7160d96d4cd3afc647b4
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2023-03-28 10:33:47 +0200

    update parameters

--- scripts/step_to_h5m.py
+++ scripts/step_to_h5m.py
@@ -9,22 +9,17 @@ import CAD_to_OpenMC.assembly as ab
 ###############################################################################
 
 # inputs
-step_filepath = "./step_files/ARE.step"
-h5m_out_filepath = os.getcwd() + '/h5m_files/ARE.h5m'
+step_filepath = "./step_files/are.step"
+h5m_out_filepath = os.getcwd() + '/h5m_files/are.h5m'
 
 # mesher config
-ab.mesher_config['min_mesh_size'] = 0.001
-ab.mesher_config['mesh_algorithm'] = 2
-#ab.mesher_config['threads'] = 6
-#ab.mesher_config['max_mesh_size'] = 1000
+ab.mesher_config['min_mesh_size'] = 0.5
+ab.mesher_config['min_mesh_size'] = 100
+ab.mesher_config['mesh_algorithm'] = 1
+ab.mesher_config['threads'] = 56
 ab.mesher_config['curve_samples'] = 100
-#ab.mesher_config['vetoed'] = [474,1157,1243,1341,1537]
-#ab.mesher_config['angular_tolerance'] = 0.09
 
-#ab.mesher_config['tolerance'] = 0.001
-
+ab.mesher_config['refine'] = 0
 # output
-a=ab.Assembly()
-a.stp_files=[step_filepath]
-a.import_stp_files()
-a.solids_to_h5m(backend='gmsh',h5m_filename=h5m_out_filepath)
+a=ab.Assembly([step_filepath],verbose=2)
+a.run(backend='stl',h5m_filename=h5m_out_filepath)
